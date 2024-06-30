ef8474c54cad08b9a33d16de893a6e39a5487c1b
MyNameIsTrez <welfje@gmail.com>
2024-06-30 17:52:52 +0200

    Add comments

--- README.md
+++ README.md
@@ -165,7 +165,7 @@ int main(void) {
 	printf("%d\n", get_person("trez")->age); // Prints "7"
 	printf("%d\n", get_person("john")->age); // Prints "42"
 	printf("%d\n", get_person("carl")->age); // Prints "69"
-	printf("%p\n", (void *)get_person("bob")); // Prints "(null)"
+	printf("%p\n", (void *)get_person("bob")); // Prints "(nil)"
 }
 ```
 
--- main.c
+++ main.c
@@ -73,8 +73,8 @@ int main(void) {
 
 	hash_persons();
 
-	printf("%d\n", get_person("trez")->age);
-	printf("%d\n", get_person("john")->age);
-	printf("%d\n", get_person("carl")->age);
-	printf("%p\n", (void *)get_person("bob"));
+	printf("%d\n", get_person("trez")->age); // Prints "7"
+	printf("%d\n", get_person("john")->age); // Prints "42"
+	printf("%d\n", get_person("carl")->age); // Prints "69"
+	printf("%p\n", (void *)get_person("bob")); // Prints "(nil)"
 }
